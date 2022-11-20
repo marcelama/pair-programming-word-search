4ef776ad833bcd35035311c8b14660c8062fa947
Marcela Ang <marcela.ang@gmail.com>
2022-11-20 16:58:58 -0500

    M2W5 - Word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  // console.log(horizontalJoin); //test
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  const horizontalReverseJoin = letters.map(ls => ls.reverse().join(''));
+  // console.log(horizontalReverseJoin); //test
+  for (let y of horizontalReverseJoin) {
+    if (y.includes(word)) return true;
+  }
+
+  const vertical = [];
+
+  for (let col = 0; col < letters[0].length; col++) {
+    let line = [];
+    for (let row = 0; row < letters.length; row++) {
+      line.push(letters[row][col]);
+    }
+    vertical.push(line);
+  }
+  const verticalJoin = vertical.map(ls => ls.join(''));
+  console.log(verticalJoin); //test
+  for (let x of verticalJoin) {
+    if (x.includes(word)) return true;
+    // else return false;
+  }
+
+  return false;
+
+};
+
+
+module.exports = wordSearch;
+
